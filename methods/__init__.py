2ef2f326358f4eb4ddbdccd8f9cbe997a351848a
PJHancock <PJHancock2@gmail.com>
2025-10-22 12:59:52 -0600

    added RhoLossIS method

--- methods/__init__.py
+++ methods/__init__.py
@@ -5,5 +5,6 @@ from .Uniform import Uniform
 from .RhoLoss import RhoLoss
 from .TrainLoss import TrainLoss
 from .RhoLossRW import RhoLossRW
+from .RhoLossIS import RhoLossIS
 
-__all__ = ["Uniform", "DivBS", "Full", "Bayesian", "RhoLoss", "TrainLoss", "RhoLossRW"]
+__all__ = ["Uniform", "DivBS", "Full", "Bayesian", "RhoLoss", "TrainLoss", "RhoLossRW", "RhoLossIS"]
