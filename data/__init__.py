3c5ac0edc642ec97cd7d5a870c74f98c0d448f55
PJHancock <PJHancock2@gmail.com>
2026-01-21 14:16:47 -0700

    Added MNIST and FashionMNIST into repo. Includes adjusting models to dynamically take in in_channels

--- data/__init__.py
+++ data/__init__.py
@@ -1,2 +1,3 @@
 from .cifar import CIFAR10, CIFAR100, CIFAR100_LT, CIFAR10_LT  #, CIFAR100_half, CIFAR10_half
+from .mnist import MNIST, FashionMNIST
 from .tinyimagenet import TinyImageNet
--- models/Small_cnn.py
+++ models/Small_cnn.py
@@ -3,8 +3,8 @@ import torch.nn as nn
 import torch.nn.functional as F
 import torchvision.models as models
 
-def create_model(m_type='small_cnn', num_classes=10, pretrained = False):
-    model = small_cnn()
+def create_model(m_type='small_cnn', in_channels=3, num_classes=10, pretrained=False):
+    model = small_cnn(in_channels=in_channels, num_classes=num_classes)  # default model
     return model
 
 class small_cnn(nn.Module):
@@ -18,6 +18,8 @@ class small_cnn(nn.Module):
         self.conv3 = nn.Conv2d(128, 256, 3)
         self.conv3_drop = nn.Dropout2d(p=dropout)
         self.fc1 = nn.Linear(64 * 4 * 4, 128)
+        # use for mnist
+        # self.fc1 = nn.Linear(256 * 1 * 1, 128)
         self.fc1_drop = nn.Dropout(dropout)
         self.fc2 = nn.Linear(128, 256)
         self.fc2_drop = nn.Dropout(dropout)
@@ -25,14 +27,23 @@ class small_cnn(nn.Module):
 
         self.pool = nn.MaxPool2d(2, 2)
 
-    def forward(self, x):
+    def forward(self, x, **kwargs):
+
+        feature = kwargs.get('need_features', False)
+
         x = F.relu(self.pool(self.conv1_drop(self.conv1(x))))
         x = F.relu(self.pool(self.conv2_drop(self.conv2(x))))
         x = F.relu(self.pool(self.conv3_drop(self.conv3(x))))
 
-        x = x.view(-1, 64 * 4 * 4)
+        #x = x.view(-1, 64 * 4 * 4)
+        x = torch.flatten(x, start_dim=1)
 
         x = F.relu(self.fc1_drop(self.fc1(x)))
         x = F.relu(self.fc2_drop(self.fc2(x)))
+        feat = x
         x = self.fc3(x)
-        return x
+        
+        if feature:
+            return x, feat
+        else:
+            return x
