351caada8ebd1e7b6b491bd3653681a1178d06f5
lukegreen109 <130334762+lukegreen109@users.noreply.github.com>
2026-05-18 13:46:58 -0600

    Expand SEEDS and update DATA paths in script

--- run_mnist_noisy.sh
+++ run_mnist_noisy.sh
@@ -2,15 +2,15 @@
 
 # Usage: ./run.sh
 
-SEEDS=(1)
+SEEDS=(1 2 3 4 5)
 DIAGNOSTICS="configs/diagnostics/snapshots_log_interval.yaml"
 
 CONFIG_DIR="configs/mnist"
 
-METHODS=("$CONFIG_DIR/method/rholoss-0.1.yaml")
+METHODS=("$CONFIG_DIR/method/"*)
 MODELS=("$CONFIG_DIR/model/lenet.yaml")
 OPTIMS=("$CONFIG_DIR/optim/adamw-320-0.001-0.01.yaml")
-DATAS=("$CONFIG_DIR/data/seed_1/mnist_noise_0.1p_v1.yaml" "$CONFIG_DIR/data/seed_1/mnist_noise_0.2p_v1.yaml" "$CONFIG_DIR/data/seed_1/mnist_noise_0.0p_v1.yaml")
+DATAS=("$CONFIG_DIR/data/mnist_noise.yaml")
 
 for SEED in "${SEEDS[@]}"; do
   for data in "${DATAS[@]}"; do
