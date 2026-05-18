26348e9fe482114928cf41c80fc30b83d3dd2a60
toddgate30 <toddgate30@gmail.com>
2026-05-18 13:41:13 -0600

    updated

--- run_mnist_noisy.sh
+++ run_mnist_noisy.sh
@@ -7,10 +7,10 @@ DIAGNOSTICS="configs/diagnostics/snapshots_log_interval.yaml"
 
 CONFIG_DIR="configs/mnist"
 
-METHODS=("$CONFIG_DIR/method/uniform-0.1.yaml")
+METHODS=("$CONFIG_DIR/method/rholoss-0.1.yaml")
 MODELS=("$CONFIG_DIR/model/lenet.yaml")
 OPTIMS=("$CONFIG_DIR/optim/adamw-320-0.001-0.01.yaml")
-DATAS=("$CONFIG_DIR/data/"*)
+DATAS=("$CONFIG_DIR/data/seed_1/mnist_noise_0.1p_v1.yaml" "$CONFIG_DIR/data/seed_1/mnist_noise_0.2p_v1.yaml" "$CONFIG_DIR/data/seed_1/mnist_noise_0.0p_v1.yaml")
 
 for SEED in "${SEEDS[@]}"; do
   for data in "${DATAS[@]}"; do
